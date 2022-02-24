c4cc7e7c35fd9933e5e5bf334029828a8ec7cc63
MMhenna <mhenna.mehdi@gmail.com>
2022-02-24 12:44:03 +0100

    commit

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,48 +1,95 @@
 #include "slide_line.h"
 
+/**
+ * slide_line - Slide and merge an array of integers
+ * @line: Pointer to the array of integers
+ * @size: Number of elements in array
+ * @direction: SLIDE_LEFT or SLIDE_RIGHT
+ * Return: 1 on success, 0 otherwise
+ */
 int slide_line(int *line, size_t size, int direction)
 {
-	int *place_here = NULL;
-	int *left = NULL;
-	int *right = NULL;
+	size_t i;
+	int change = 0;
 
 	if (direction == SLIDE_LEFT)
 	{
-		place_here = line;
-		left = place_here;
-
-		while (left < line + (size - 1))
+		slide_left(line, size);
+		for (i = 1; i < size && line[i] != 0; i++)
 		{
-			while (*left == 0 && left < line + (size - 1))
+			if (line[i - 1] == line[i])
 			{
-				left++;
+				line[i - 1] *= 2;
+				line[i] = 0;
+				change = 1;
 			}
-			right = left + 1;
-			while (right < line + (size))
-			{
-				if (*right == *left)
-				{
-					*place_here = *left * 2;
-					if (place_here != left)
-					{
-						*left = 0;
-					}
-					*right = 0;
-					place_here++;
-					break;
-				}
-				else
-				{
-					right++;
-				}
-			}
-			left++;
 		}
-		if (*(line + size - 1) && !*place_here)
+		if (change)
+			slide_left(line, size);
+	}
+	else if (direction == SLIDE_RIGHT)
+	{
+		slide_right(line, size);
+		for (i = size - 1; i > 0 && line[i - 1] != 0; i--)
 		{
-			*place_here = *(line + size - 1);
-			*(line + size - 1) = 0;
+			if (line[i - 1] == line[i])
+			{
+				line[i] *= 2;
+				line[i - 1] = 0;
+				change = 1;
+			}
+		}
+		if (change)
+			slide_right(line, size);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (1);
+}
+
+/**
+ * slide_left - Slide an array of integers to the left
+ * @line: Pointer to the array of integers
+ * @size: Number of elements in array
+ */
+void slide_left(int *line, size_t size)
+{
+	int i, temp, current = 0;
+
+	for (i = 0; i < (int) size && current < (int) size; i++)
+	{
+		while (line[current] != 0)
+			current++;
+		if (line[i] != 0 && i > current)
+		{
+			temp = line[i];
+			line[i] = line[current];
+			line[current] = temp;
 		}
 	}
-	return (1);
+}
+
+/**
+ * slide_right - Slide an array of integers to the right
+ * @line: Pointer to the array of integers
+ * @size: Number of elements in array
+ */
+void slide_right(int *line, size_t size)
+{
+	int i, temp, current = size - 1;
+
+	for (i = size - 1; i >= 0 && current >= 0; i--)
+	{
+		while (current > 0 && line[current] != 0)
+			current--;
+		if (line[i] != 0 && i < (int) current)
+		{
+			temp = line[i];
+			line[i] = line[current];
+			line[current] = temp;
+		}
+	}
 }
