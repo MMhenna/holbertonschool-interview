2b4785bffb8cde1d673127ae952e1367d3675ecc
MMhenna <mhenna.mehdi@gmail.com>
2022-06-23 20:45:38 +0100

    commit

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
-Task: Change comes from within
-Given a pile of coins of different values,
-determine the fewest number of coins needed to
-meet a given amount total
+Change comes from within
 """
 
 
@@ -11,6 +8,13 @@ def makeChange(coins, total):
     """
     Given a pile of coins of different values, determine the fewest
     number of coins needed to meet a given amount total.
+    Return: fewest number of coins needed to meet total
+        - If total is 0 or less, return 0
+        - If total cannot be met by any number of coins you have, return -1
+        - Coins is a list of the values of the coins in your possession
+        - The value of a coin will always be an integer greater than 0
+        - You can assume you have an infinite number of each denomination of
+        coin in the list
     """
     if total <= 0:
         return 0
